727ff341c29daadff7a8232ae9bd926d2c9870bb
Johnson Giang <johnson.giang21@gmail.com>
2024-05-02 18:51:05 -0600

    dasdsa

--- pages/project.js
+++ pages/project.js
@@ -1,35 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from '@/components/Header';  // Make sure the path is correct based on your project structure.
 import '../app/globals.css';
 
-const projects = () => {
-    const [text, setText] = useState('');
-    const fullText = 'print(Hello Resume!)';
-
-    useEffect(() => {
-        let index = 0;
-        const intervalId = setInterval(() => {
-            setText((prevText) => {
-                const newText = prevText + fullText[index];
-                index += 1;
-                if (index >= fullText.length) {
-                    clearInterval(intervalId);
-                }
-                return newText;
-            });
-        }, 125);
-
-        return () => clearInterval(intervalId);
-    }, []);
-
+const AboutMe = () => {
     return (
         <div>
             <Header />
             <main>
-                <p>This is my project page.</p>
+                <p>This is the About Me page content.</p>
             </main>
         </div>
     );
 }
 
-export default projects;
+export default AboutMe;
