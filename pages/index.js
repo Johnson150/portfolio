f74ff827d17c6968c3c984aa60bd85dabfd3be7a
Johnson Giang <johnson.giang21@gmail.com>
2024-05-03 06:15:50 -0600

    dads

--- pages/index.js
+++ pages/index.js
@@ -29,14 +29,14 @@ const AboutMe = () => {
                 <h1 className="text-3xl font-bold text-green-500">{text}</h1>
                 <section className="p-20">
                     <h2 className="text-2xl font-bold text-green-500">About me</h2>
-                    <div className='p-5 flex justify-center items-center'>
+                    <div className="p-5 flex justify-center items-center">
                         <img src="/Profile pic.jpg" alt="profile pic" className="w-auto h-auto rounded-lg shadow-lg slide-up" />
                     </div>
                     <p className="mt-2 text-lg slide-up">
                         Hi, my name is Johnson! Growing up, I always had a desire to be creative in different outlets such as being a part of drama productions and choir.
                         Looking for a more creative outlet in my post-secondary life, I decided to pursue the Information Technology and Software Development diploma at the Southern Alberta Institute of Technology.
                         Being able to create applications has always interested me, and going to SAIT really helped me learn lots of skills that will help me in my future professional life.
-                        In my year and a half at SAIT, I have learned many programming languages such as PL/SQL, Python, Java, and C#. I've also learned and worked with software such as Figma and SIM (Software Ideas Modeler).
+                        In my year and a half at SAIT, I have learned many programming languages such as PL/SQL, Python, Java, and C#. I&apos;ve also learned and worked with software such as Figma and SIM (Software Ideas Modeler).
                         Through my studies, I have found myself absolutely fascinated with the world of web development. I have learned crucial skills in HTML, CSS, JavaScript, React.js, and Next.js.
                         After my studies, I plan to pursue a career in Web Development.
                     </p>
@@ -67,7 +67,6 @@ const AboutMe = () => {
                     <img src="/react logo.png" alt="react logo" className="w-1/6 h-1/6 rounded-lg shadow-lg slide-transition mx-auto"></img>
                     <img src="/next js logo.png" alt="next logo" className="w-1/6 h-1/6 rounded-lg shadow-lg slide-transition mx-auto"></img>
                 </div>
-
             </main >
         </div >
 
--- pages/project.js
+++ pages/project.js
@@ -4,7 +4,7 @@ import '../app/globals.css';
 
 const Project = () => {
     const [text, setText] = useState('');
-    const fullText = 'print("Hello Projects!")';
+    const fullText = 'print("Hello Project!")';
 
     useEffect(() => {
         let index = 0;
@@ -36,10 +36,10 @@ const Project = () => {
                         In addition to these player management capabilities, the app boasts a fully functional sign-up and login page, ensuring a secure and personalized user experience. Furthermore, the inclusion of a trade simulator adds an exciting dynamic, allowing users to simulate player trades by selecting up to two players to exchange.
                     </p>
                     <p className="mt-2 text-lg slide-transition">This is a ongoing project with even more functionality in the near future!</p>
-                    <div className='p-5 flex justify-center items-center'>
+                    <div className="p-5 flex justify-center items-center">
                         <img src="/NBA Player App.png" alt="Unit Converter Preview" className="w-2/6 h-1/2 rounded-lg shadow-lg slide-transition" ></img>
                     </div>
-                    <div className='p-5'>
+                    <div className="p-5">
                         <a href="https://github.com/Johnson150/basketballapp" className="mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Code</a>
                         <a href="https://basketballapp.vercel.app/" className="mx-4 mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Site</a>
                     </div>
@@ -47,10 +47,10 @@ const Project = () => {
                 <div className="p-20">
                     <h2 className="text-2xl font-bold text-green-500">IMR Movie App</h2>
                     <p className="mt-2 text-lg slide-transition">This app allows users to add movies, remove movies and edit movies.</p>
-                    <div className='p-5 flex justify-center items-center'>
+                    <div className="p-5 flex justify-center items-center">
                         <img src="/IMR movie preview.png" alt="IMR Movie App Preview" className="w-2/6 h-1/2 rounded-lg shadow-lg slide-transition" ></img>
                     </div>
-                    <div className='p-5'>
+                    <div className="p-5">
                         <a href="https://github.com/NavkiratPuri/asg3movies" className="mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Code</a>
                         <a href="https://asg3movies.vercel.app/" className="mx-4 mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Site</a>
                     </div>
@@ -58,10 +58,10 @@ const Project = () => {
                 <div className="p-20">
                     <h2 className="text-2xl font-bold text-green-500">Unit converter</h2>
                     <p className="mt-2 text-lg slide-transition">This app allows users to convert temperature, distance and weight.</p>
-                    <div className='p-5 flex justify-center items-center'>
+                    <div className="p-5 flex justify-center items-center">
                         <img src="/Unit Converter.png" alt="Unit Converter Preview" className="w-2/6 h-1/2 rounded-lg shadow-lg slide-transition" ></img>
                     </div>
-                    <div className='p-5'>
+                    <div className="p-5">
                         <a href="https://github.com/Johnson150/web-dev-2-assignment.github.io" className="mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Code</a>
                         <a href="https://johnson150.github.io/web-dev-2-assignment.github.io/" className="mx-4 mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Site</a>
                     </div>
@@ -69,10 +69,10 @@ const Project = () => {
                 <div className="p-20">
                     <h2 className="text-2xl font-bold text-green-500">Raj Robot Rental</h2>
                     <p className="mt-2 text-lg slide-transition">This app allows users to see the price of how much it cost to rent out two different models of robots by duration and rate.</p>
-                    <div className='p-5 flex justify-center items-center'>
+                    <div className="p-5 flex justify-center items-center">
                         <img src="/raj robot rental.png" alt="Unit Converter Preview" className="w-2/6 h-1/2 rounded-lg shadow-lg slide-transition" ></img>
                     </div>
-                    <div className='p-5'>
+                    <div className="p-5">
                         <a href="https://github.com/Johnson150/Raj-Robot-Rental" className="mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Code</a>
                         <a href="https://johnson150.github.io/Raj-Robot-Rental/" className="mx-4 mt-12 bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-300 slide-up">Site</a>
                     </div>
